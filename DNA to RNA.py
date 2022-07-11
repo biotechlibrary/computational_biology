952477a4b75027f0ab46dcc826b1703b5265fe5e
biotechlibrary <101683451+biotechlibrary@users.noreply.github.com>
2022-07-10 23:24:46 -0700

    DNA to RNA converter
    Convert or count without all the brain power.

---
+++ DNA to RNA.py
@@ -0,0 +1,20 @@
+#Pau
+#July 10th 2022
+#Assignment 5
+
+    #Get user input
+
+DNA=input("Enter a short DNA sequence between 10 and 20bp") 
+dna_length =len(DNA)
+
+print(dna_length)
+
+    #Count the input
+
+countA=DNA.count('A')
+print(countA)
+
+    #DNA to RNA
+
+DNA=input
+print(DNA.replace("T", "U"))
