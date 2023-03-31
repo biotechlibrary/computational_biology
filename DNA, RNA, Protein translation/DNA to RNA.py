1c4ed6ff157ead90ac73adc76af8ca240cb48235
Pau Rotger Gonzalez <101683451+biotechlibrary@users.noreply.github.com>
2023-03-31 14:56:08 -0700

    Update DNA to RNA.py

--- DNA, RNA, Protein translation/DNA to RNA.py
+++ DNA, RNA, Protein translation/DNA to RNA.py
@@ -1,20 +1,17 @@
-#Pau
-#July 10th 2022
-#Assignment 5
 
-    #Get user input
+#Get user input
 
 DNA=input("Enter a short DNA sequence between 10 and 20bp") 
 dna_length =len(DNA)
 
 print(dna_length)
 
-    #Count the input
+ #Count the input
 
 countA=DNA.count('A')
 print(countA)
 
-    #DNA to RNA
+ #DNA to RNA
 
 DNA=input
 print(DNA.replace("T", "U"))
